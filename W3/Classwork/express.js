60e3232650ffdc7b09891dbb1ef56679ed403f4b
Choeng-Rayu <choengrayu307@gmail.com>
2025-05-12 16:15:29 +0700

    push

--- W3/Classwork/express.js
+++ W3/Classwork/express.js
@@ -6,3 +6,19 @@ app.get('/', (req, res)=>{
 app.listen(3000, (req, res)=>{
     console.log('Server is running at http://localhost:3000');
 });
+app.post('/contact', (req, res)=>{
+    res.send('Form submitted');
+});
+app.post('/submit' , (req, res)=>{
+    res.send('form submitted');
+});
+
+app.use(express.urlencoded({extended: true}));
+app.get('/contact', (req, res)=>{
+    res.send(`
+        <form method="POST" action="/contact">
+            <input type="text" name="name" placeholder="Your name" />
+            <button type="submit">Submit</button>
+        </form>
+    `);
+});
