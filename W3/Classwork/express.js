54b7013937a266a19a2eb70c6681d64ca7c522b7
Choeng-Rayu <choengrayu307@gmail.com>
2025-05-13 10:01:09 +0700

    push

--- W3/Classwork/express.js
+++ W3/Classwork/express.js
@@ -3,22 +3,37 @@ const app = express();
 app.get('/', (req, res)=>{
     res.send('Welcome to the Home Page');
 });
+// app.listen(3000, (req, res)=>{
+//     console.log('Server is running at http://localhost:3000');
+// });
+// app.post('/contact', (req, res)=>{
+//     res.send('Form submitted');
+// });
+
+// app.use(express.urlencoded({extended: true}));
+// app.get('/contact', (req, res)=>{
+//     res.send(`
+//         <form method="POST" action="/contact">
+//             <input type="text" name="name" placeholder="Your name" />
+//             <button type="submit">Submit</button>
+//         </form>
+//     `);
+// });
+// app.use(express.json());
+// app.post ('/user' ,  (req, res)=>{
+//     console.log(req.body);
+//     res.send('form submitted');
+// });
+
+
+function logger(req, res, next){
+    console.log(`[${new Date().toISOString()}]${req.method} ${req.url}`);
+    next();
+}
+app.use(logger);
+app.get('/hello', (req, res)=>{
+    res.send('Welcome to the Home Page');
+});
 app.listen(3000, (req, res)=>{
     console.log('Server is running at http://localhost:3000');
-});
-app.post('/contact', (req, res)=>{
-    res.send('Form submitted');
-});
-app.post('/submit' , (req, res)=>{
-    res.send('form submitted');
-});
-
-app.use(express.urlencoded({extended: true}));
-app.get('/contact', (req, res)=>{
-    res.send(`
-        <form method="POST" action="/contact">
-            <input type="text" name="name" placeholder="Your name" />
-            <button type="submit">Submit</button>
-        </form>
-    `);
 });
