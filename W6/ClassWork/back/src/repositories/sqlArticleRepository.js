82e48efdd01133151e57e3e85a27c81e4a087805
Choeng-Rayu <choengrayu307@gmail.com>
2025-06-09 17:19:47 +0700

    push

--- W6/ClassWork/back/src/repositories/sqlArticleRepository.js
+++ W6/ClassWork/back/src/repositories/sqlArticleRepository.js
@@ -4,8 +4,8 @@
 // -  Perfrom the SQL querries to implement the bellow API
 //
 
- const mysql = require('mysql2/promise');
- const database = require('../database');
+import mysql from 'mysql2/promise';
+// const database = require('../database'); // Not needed in ES module, use import instead
 //  const pool = mysql.createPool({ ... });
  import {pool } from '../utils/database.js';
 
@@ -13,8 +13,9 @@
 export async function getArticles() {
     // TODO
     try{
-        const [rows] = await pool.query('select * from airline_database2');
-        return send.json(rows);
+        const [rows] = await pool.query('select * from airlines;');
+        // return send.json(rows);
+        return rows;
     }
     catch(err){
         console.error('Error fetching airlineDb:' , err);
@@ -23,12 +24,23 @@ export async function getArticles() {
 
 // Get one article by ID
 export async function getArticleById(id) {
-    // TODO
+    try {
+        const [rows] = await pool.query('SELECT * FROM airlines WHERE id = ?', [id]);
+        return rows[0]; // Return the first (and only) matching article
+    } catch (err) {
+        console.error('Error fetching article by ID:', err);
+        throw err;
+    }
 }
 
 // Create a new article
 export async function createArticle(article) {
     // TODO
+    try{
+        const [result] = await pool.query('INSERT INTO AIRLINES values (AirlineID, Name, Country)')
+    }catch(err){
+        console.error('Error creating article:', err);
+    }
 }
 
 // Update an article by ID
